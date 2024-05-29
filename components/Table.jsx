d1a6c0350f0d8bed4baaa14359d52286976999cc
ParasuramRam <parasuramp@kpmg.com>
2024-05-30 00:42:28 +0530

    Table component has apply button. On click of apply button it shows the name of the selected item in alert. (#4)

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,11 +1,18 @@
-import React,{useState} from "react"
+import React,{useState,useEffect} from "react"
 export default function Table({header,data})
 {
+     const[selectedRow,setSelectedrow]=useState({})
      if(!Array.isArray(data)||data.length===0)
      {
           return (<p>No data available</p>)  
      }
      const headers=Object.keys(data[0])
+     useEffect(()=>{
+     },[selectedRow])
+
+     const applybtnOnclick=()=>{
+       window.alert(selectedRow['name'])
+     }
 
      return (
         <div className="my-3 p-3">
@@ -22,11 +29,15 @@ export default function Table({header,data})
                 </thead>
                 <tbody>
                     {data.map((row,index)=>(
-                        <tr key={index} className="bg-gray-50 hover:bg-gray-200 hover:cursor-pointer transform transition-transform duration-200"
-                            style={{transformOrigin:'center'}}>
+                        <tr key={index} className={row['id']===selectedRow['id']?"bg-blue-300":"bg-gray-50 hover:bg-gray-200 hover:cursor-pointer transform transition-transform duration-200"}
+                            style={{transformOrigin:'center'}} 
+                            onClick={()=>{setSelectedrow({...row})}}>
                             {headers.map((header)=>(
-                                <td key={header} className={`px-4 py-2 border-b border-gray-200 text-sm  ${header==='name'?'text-blue-500 hover:underline':'text-gray-700'}`}
-                                    onClick={header==='name'?()=>{window.alert(row[header])}:()=>{}}>
+                                <td key={header} className={`px-4 py-2 border-b border-gray-200 text-sm  
+                                ${header==='name'?row['id']===selectedRow['id']?'text-white hover:underline':'text-blue-500 hover:underline':
+                                row['id']===selectedRow['id']?'text-white':'text-gray-700'}`}
+                                    // onClick={header==='name'?()=>{window.alert(row[header])}:()=>{}}
+                                >
                                     {row[header]}
                                 </td>
                             ))}
@@ -34,6 +45,10 @@ export default function Table({header,data})
                     ))}
                 </tbody>
             </table>
+            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white mt-4 py-2 px-4 border border-blue-500 hover:border-transparent rounded" 
+            onClick={()=>{applybtnOnclick()}}>
+            Apply
+            </button>
         </div>
      )
 }
